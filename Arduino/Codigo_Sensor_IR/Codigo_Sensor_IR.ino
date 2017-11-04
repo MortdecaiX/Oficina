4f568cc509ea91d143650410de269aa94afbc50f
VitorVinicius <vitor.v.gomes@live.com>
2017-11-04 03:16:33 -0200

    Ajustes no código do Arduino

--- Arduino/Codigo_Sensor_IR/Codigo_Sensor_IR.ino
+++ Arduino/Codigo_Sensor_IR/Codigo_Sensor_IR.ino
@@ -5,10 +5,14 @@ int IRSense3 = A3;
 int IRSense4 = A4;
 int IRSense5 = A5;
 int analogPorts[] = {IRSense0,IRSense1,IRSense2,IRSense3,IRSense4,IRSense5};
-int numeroControlador =-1;
+int sensibilidade =-1;
+
+int maxAnalogSensors = 0;
+
 void setup() 
 {
-  //Programe as portas de 2 até 9 na forma de um vetor de 8 bits para corresponder ao numero do controlador. Programa até 256:11111111
+  //Programe as portas de 2 até 6 na forma de um vetor de 5 bits para corresponder a sensibilidade do controlador. Programe até 32:11111
+  //Programe as portas de 7 até 9 na forma de um vetor de 3 bits para corresponder a sensibilidade do controlador. Programe até 6
   //Cada porta analogica do arduino corresponde à uma vaga. No máximo 6 vagas podem ser geranciadas sem multiplexação;
   pinMode(2, INPUT);//+1
   pinMode(3, INPUT);//+2
@@ -18,7 +22,8 @@ void setup()
   pinMode(7, INPUT);//+32
   pinMode(8, INPUT);//+64
   pinMode(9, INPUT);//+128
-  numeroControlador = readPins();
+  sensibilidade = readPins();
+  maxAnalogSensors = readMaxAnalogPins();
   pinMode(IRSense0, INPUT);
   pinMode(IRSense1, INPUT);
   pinMode(IRSense2, INPUT);
@@ -32,17 +37,24 @@ void setup()
 
 void loop() 
 {
-  Serial.print("<controlador id=\"");
-  Serial.print(numeroControlador);
+  Serial.print("<controlador sensibilidade=\"");
+  Serial.print(sensibilidade);
+  Serial.print("\" sensores=\">");   
+  Serial.print(maxAnalogSensors);
   Serial.print("\">");   
-  for(int i=0; i<6;i++){
+  for(int i=0; (i<maxAnalogSensors) && (i<6);i++){
       int value = analogRead(analogPorts[i]);
-      float dist = 0.0089*value -4.0853;
+      float dist = 0.03125*value;
       Serial.print("<vaga><numero>");   
       Serial.print(i+1);    
       Serial.print("</numero><estado>");
-      Serial.print(dist<4.5?1:0);
-      Serial.print("</estado></vaga>");
+      Serial.print((dist<sensibilidade)?1:0);
+      Serial.print("</estado>");
+
+      Serial.print("<distancia>");
+      Serial.print(dist);
+      Serial.print("</distancia></vaga>");
+      
       
   }    
 Serial.println("</controlador>");
@@ -50,31 +62,36 @@ delay(1000);
 }
 
 int readPins(){//le a programação do numero da vaga
-  int numeroControlador=0;
-  if((digitalRead(9) == HIGH)){
-          numeroControlador+=128;
-        }
-      if((digitalRead(8) == HIGH)){
-          numeroControlador+=64;
-        }
-      if((digitalRead(7) == HIGH)){
-          numeroControlador+=32;
-        }
+  int sensibilidade=0;
       if((digitalRead(6) == HIGH)){
-          numeroControlador+=16;
+          sensibilidade+=16;
         }
       if((digitalRead(5) == HIGH)){
-          numeroControlador+=8;
+          sensibilidade+=8;
         }
       if((digitalRead(4) == HIGH)){
-          numeroControlador+=4;
+          sensibilidade+=4;
         }
       if((digitalRead(3) == HIGH)){
-          numeroControlador+=2;
+          sensibilidade+=2;
         }
       if((digitalRead(2) == HIGH)){
-          numeroControlador+=1;
+          sensibilidade+=1;
         }
-    return numeroControlador;
+    return sensibilidade;
+  
+  }
+  int readMaxAnalogPins(){//le a programação do numero da vaga
+  int qnt=0;
+  if((digitalRead(9) == HIGH)){
+          qnt+=4;
+        }
+      if((digitalRead(8) == HIGH)){
+          qnt+=2;
+        }
+      if((digitalRead(7) == HIGH)){
+          qnt+=1;
+        }
+    return qnt;
   
   }
